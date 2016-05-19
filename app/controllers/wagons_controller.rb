29650a3c1977d63e557d9276172defd4036d24a7
Anti-k <romanslyzebniy@gmail.com>
2016-05-19 15:51:20 +0300

    validates inclusion added and rubocoped

--- app/controllers/wagons_controller.rb
+++ app/controllers/wagons_controller.rb
@@ -1,15 +1,20 @@
 class WagonsController < ApplicationController
   before_action :set_wagon, only: [:show, :edit, :update, :destroy]
+
   def index
     @wagons = Wagon.all
-  end  
+  end
+
   def show
   end
+
   def new
     @wagon = Wagon.new
   end
+
   def edit
   end
+
   def update
     if @wagon.update(wagon_params)
       redirect_to @wagon
@@ -17,6 +22,7 @@ class WagonsController < ApplicationController
       render :edit
     end
   end
+
   def create
     @wagon = Wagon.new(wagon_params)
     if @wagon.save
@@ -25,15 +31,20 @@ class WagonsController < ApplicationController
       render :new
     end
   end
+
   def destroy
     @wagon.destroy
     redirect_to wagons_path
   end
+
   private
+
   def set_wagon
     @wagon = Wagon.find(params[:id])
   end
+
   def wagon_params
     params.require(:wagon).permit(:types, :upper, :lower, :train_id)
   end
+  
 end
--- app/models/wagon.rb
+++ app/models/wagon.rb
@@ -1,4 +1,5 @@
 class Wagon < ActiveRecord::Base
   belongs_to :train
   validates :types, presence: true
+  validates :upper, :lower, inclusion: { in: (0..100)}
 end
