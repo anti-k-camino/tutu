74420f1da72b02a02687a3e3d6aace0bf1b6ff20
Anti-k <romanslyzebniy@gmail.com>
2016-05-25 14:19:15 +0300

    update added

--- app/models/stations_route.rb
+++ app/models/stations_route.rb
@@ -3,11 +3,6 @@ class StationsRoute < ActiveRecord::Base
   belongs_to :route  
 
   validates :station_id, uniqueness: { scope: :route_id }
-  validates :number, uniqueness:{ scope: :route_id } 
-  
-  def update_number(num)
-    self.number = num
-    self.save
-  end 
+  validates :number, uniqueness:{ scope: :route_id }   
   
 end
