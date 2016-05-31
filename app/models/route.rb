d61fb195bc03d8ba91d3b18c4a52d6e5d846f052
Anti-k <romanslyzebniy@gmail.com>
2016-05-31 16:56:06 +0300

    serch by id is provided

--- app/models/route.rb
+++ app/models/route.rb
@@ -11,17 +11,21 @@ class Route < ActiveRecord::Base
     #res = self.joins(:stations).where("stations.name LIKE ?" ,"%#{search_begin}%" ).uniq 
     #res1 = self.joins(:stations).where("stations.name LIKE ?" ,"%#{search_end}%" ).uniq 
 
-    #SELECT DISTINCT `routes`.* FROM `routes` INNER JOIN `stations_routes`
+    #SELECT DISTINCT `routes`.* FROM `routes` INNER JOIN `stations_routes`# DISTINCT may be useless
     #ON `stations_routes`.`route_id` = `routes`.`id` INNER JOIN `stations`
     #ON `stations`.`id` = `stations_routes`.`station_id`
     #WHERE (`stations`.`name` LIKE '%Одесса%' OR `stations`.`name` LIKE '%Киев%')
     #GROUP BY routes.id
     #HAVING COUNT(`routes`.*) > 1;
-    
+
     res = self.joins(:stations).where("station_id = ?", "#{search_begin}")
     res1 = self.joins(:stations).where("station_id = ?", "#{search_end}")
     res & res1        
   end 
+
+  def station(id)
+    stations.find(id)
+  end
 
   private
 
