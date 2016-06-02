b8515f96bb4a7eb3a70ef4c103c47ac6c410f432
Anti-k <romanslyzebniy@gmail.com>
2016-06-02 12:06:38 +0300

    partial added

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,20 @@
 class SearchesController < ApplicationController
+  before_action :load_stations, only:[:show]
+
   def new
   end
 
   def show
-    @routes = Route.search(params[:search][:start_id], params[:search][:finish_id])
+    @routes = Route.search(params[:search][:start_id], params[:search][:finish_id])    
+  end
+
+ # def arrival
+ # end 
+ # def departing
+ # end
+
+  private
+  def load_stations
     @start = Station.find(params[:search][:start_id])
     @finish = Station.find(params[:search][:finish_id])
   end
