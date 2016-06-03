afdd6497dd3ae86b856ff4dbc5a8e05463cae6c3
Anti-k <romanslyzebniy@gmail.com>
2016-06-04 01:49:54 +0300

    unusefull validation on Time type is removed

--- app/models/stations_route.rb
+++ app/models/stations_route.rb
@@ -2,13 +2,4 @@ class StationsRoute < ActiveRecord::Base
   belongs_to :station
   belongs_to :route
   validates :station_id, uniqueness: { scope: :route_id }
-  validate :check_schedule, on: :update_station_attributes # НЕ РАБОТАЕТ НА UPDATE, на CREATE таки да работает
-
-  private
-
-  def check_schedule
-    unless arrival.class == ActiveSupport::TimeWithZone && departing.class == ActiveSupport::TimeWithZone
-      route.errors[:base] << 'Неверный формат времени!'
-    end
-  end
 end
