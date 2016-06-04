1826f0f82889e10f8992fe487c78aff5414efc1c
Anti-k <romanslyzebniy@gmail.com>
2016-06-04 11:03:34 +0300

    show tickets is fixed

--- db/schema.rb
+++ db/schema.rb
@@ -11,73 +11,76 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_602_064_924) do
-  create_table 'carriages', force: :cascade do |t|
-    t.integer  'number'
-    t.integer  'top_seats'
-    t.integer  'bottom_seats'
-    t.integer  'side_top_seats'
-    t.integer  'side_bottom_seats'
-    t.integer  'train_id'
-    t.string   'type'
-    t.datetime 'created_at',        null: false
-    t.datetime 'updated_at',        null: false
-    t.integer  'sitonly_seats'
+ActiveRecord::Schema.define(version: 20160602064924) do
+
+  create_table "carriages", force: :cascade do |t|
+    t.integer  "number"
+    t.integer  "top_seats"
+    t.integer  "bottom_seats"
+    t.integer  "side_top_seats"
+    t.integer  "side_bottom_seats"
+    t.integer  "train_id"
+    t.string   "type"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "sitonly_seats"
   end
 
-  create_table 'routes', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "routes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'stations', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'stations_routes', force: :cascade do |t|
-    t.integer  'station_id'
-    t.integer  'route_id'
-    t.integer  'position'
-    t.datetime 'departing'
-    t.datetime 'arrival'
+  create_table "stations_routes", force: :cascade do |t|
+    t.integer  "station_id"
+    t.integer  "route_id"
+    t.integer  "position"
+    t.datetime "departing"
+    t.datetime "arrival"
+    t.integer  "number",     default: 0
   end
 
-  create_table 'tickets', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'train_id'
-    t.integer  'user_id'
-    t.integer  'start_station_id'
-    t.integer  'finish_station_id'
-    t.string   'name'
-    t.string   'surname'
+  create_table "tickets", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "train_id"
+    t.integer  "user_id"
+    t.integer  "start_station_id"
+    t.integer  "finish_station_id"
+    t.string   "name"
+    t.string   "surname"
   end
 
-  create_table 'trains', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'station_id'
-    t.integer  'route_id'
-    t.boolean  'head', default: true
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "station_id"
+    t.integer  "route_id"
+    t.boolean  "head",       default: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'wagons', force: :cascade do |t|
-    t.string   'types'
-    t.integer  'upper'
-    t.integer  'lower'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'train_id'
+  create_table "wagons", force: :cascade do |t|
+    t.string   "types"
+    t.integer  "upper"
+    t.integer  "lower"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "train_id"
   end
+
 end
