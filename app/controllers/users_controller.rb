ab1f06d354782d178c1b8b0a1f83840e1d1ff872
Anti-k <romanslyzebniy@gmail.com>
2016-06-08 12:22:52 +0300

    unuseful controllers and actions in UsersController are removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,38 +1,7 @@
-class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
+class UsersController < ApplicationController 
 
-  def new
-    @user = User.new
-  end
-
-  def edit
-  end
-
-  def update
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit
-    end
-  end
-
-  def create
-    @user = User.new(user_params)
-
-    if @user.save
-      redirect_to @user
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name)
-  end
-
-  def set_user
+  def show
     @user = User.find(params[:id])
-  end
+  end 
+   
 end
