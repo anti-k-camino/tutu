73209e18fed798c74c56e99da19ba16b5d077ba9
Anti-k <romanslyzebniy@gmail.com>
2016-06-09 17:18:15 +0300

    some localization added

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,10 +1,10 @@
 class Carriage < ActiveRecord::Base
   belongs_to :train
 
-  validates :number, presence: true, uniqueness: { scope: :train_id }  
+  validates :number, presence: true, uniqueness: { scope: :train_id }
 
   scope :coupe_carriages, -> { where(type: 'CoupeCarriage') }
   scope :economy_carriages, -> { where(type: 'EconomyCarriage') }
   scope :sv_carriages, -> { where(type: 'SvCarriage') }
-  scope :sitonly_carriages, -> { where(type: 'SitonlyCarriage') }  
+  scope :sitonly_carriages, -> { where(type: 'SitonlyCarriage') }
 end
