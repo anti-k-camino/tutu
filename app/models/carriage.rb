75b77da8f3710b994ed9345da79b72b041ef4e93
Anti-k <romanslyzebniy@gmail.com>
2016-06-09 15:12:25 +0300

    locale added

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -1,18 +1,10 @@
 class Carriage < ActiveRecord::Base
   belongs_to :train
 
-  validates :number, presence: true, uniqueness: { scope: :train_id }
-
-  # before_validation :set_number
+  validates :number, presence: true, uniqueness: { scope: :train_id }  
 
   scope :coupe_carriages, -> { where(type: 'CoupeCarriage') }
   scope :economy_carriages, -> { where(type: 'EconomyCarriage') }
   scope :sv_carriages, -> { where(type: 'SvCarriage') }
-  scope :sitonly_carriages, -> { where(type: 'SitonlyCarriage') }
-
-  private
-
-  def set_number
-    self.number = train.train_last ? (train.train_last.number + 1) : 1
-  end
+  scope :sitonly_carriages, -> { where(type: 'SitonlyCarriage') }  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,87 +11,89 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_606_103_019) do
-  create_table 'carriages', force: :cascade do |t|
-    t.integer  'number'
-    t.integer  'top_seats'
-    t.integer  'bottom_seats'
-    t.integer  'side_top_seats'
-    t.integer  'side_bottom_seats'
-    t.integer  'train_id'
-    t.string   'type'
-    t.datetime 'created_at',        null: false
-    t.datetime 'updated_at',        null: false
-    t.integer  'sitonly_seats'
+ActiveRecord::Schema.define(version: 20160606103019) do
+
+  create_table "carriages", force: :cascade do |t|
+    t.integer  "number"
+    t.integer  "top_seats"
+    t.integer  "bottom_seats"
+    t.integer  "side_top_seats"
+    t.integer  "side_bottom_seats"
+    t.integer  "train_id"
+    t.string   "type"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "sitonly_seats"
   end
 
-  create_table 'routes', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
+  create_table "routes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
-  create_table 'stations', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "stations", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'stations_routes', force: :cascade do |t|
-    t.integer  'station_id'
-    t.integer  'route_id'
-    t.integer  'number', default: 0
-    t.integer  'position'
-    t.datetime 'departing'
-    t.datetime 'arrival'
+  create_table "stations_routes", force: :cascade do |t|
+    t.integer  "station_id"
+    t.integer  "route_id"
+    t.integer  "number",     default: 0
+    t.integer  "position"
+    t.datetime "departing"
+    t.datetime "arrival"
   end
 
-  create_table 'tickets', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'train_id'
-    t.integer  'user_id'
-    t.integer  'start_station_id'
-    t.integer  'finish_station_id'
-    t.string   'name'
-    t.string   'surname'
+  create_table "tickets", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "train_id"
+    t.integer  "user_id"
+    t.integer  "start_station_id"
+    t.integer  "finish_station_id"
+    t.string   "name"
+    t.string   "surname"
   end
 
-  create_table 'trains', force: :cascade do |t|
-    t.string   'number'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'station_id'
-    t.integer  'route_id'
-    t.boolean  'head', default: true
+  create_table "trains", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "station_id"
+    t.integer  "route_id"
+    t.boolean  "head",       default: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string   'name'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.string   'email',                  default: '',    null: false
-    t.string   'encrypted_password',     default: '',    null: false
-    t.string   'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.string   'confirmation_token'
-    t.datetime 'confirmed_at'
-    t.datetime 'confirmation_sent_at'
-    t.string   'unconfirmed_email'
-    t.boolean  'admin', default: false
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.boolean  "admin",                  default: false
   end
 
-  add_index 'users', ['email'], name: 'index_users_on_email', unique: true
-  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
-  create_table 'wagons', force: :cascade do |t|
-    t.string   'types'
-    t.integer  'upper'
-    t.integer  'lower'
-    t.datetime 'created_at'
-    t.datetime 'updated_at'
-    t.integer  'train_id'
+  create_table "wagons", force: :cascade do |t|
+    t.string   "types"
+    t.integer  "upper"
+    t.integer  "lower"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "train_id"
   end
+
 end
