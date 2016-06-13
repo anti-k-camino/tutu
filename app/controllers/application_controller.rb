e4e1fbcaa78430e9921723dcc3da3670a5536c07
anti-k-camino <anti.k.camino@gmail.com>
2016-06-13 12:06:43 +0300

    Ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 class ApplicationController < ActionController::Base
-  Я вообще сильно сомневаюсь в правильности добавления экшена в юсер, поскольку нарушение SOLID 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
