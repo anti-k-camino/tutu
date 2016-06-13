42ea52b8b15c50f4501f02391c4a75240adb6359
Anti-k <romanslyzebniy@gmail.com>
2016-06-13 20:54:09 +0300

    index to STI carriages added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613165834) do
+ActiveRecord::Schema.define(version: 20160613174941) do
 
   create_table "carriages", force: :cascade do |t|
     t.integer  "number"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20160613165834) do
     t.datetime "updated_at",        null: false
     t.integer  "sitonly_seats"
   end
+
+  add_index "carriages", ["id", "type"], name: "index_carriages_on_id_and_type"
 
   create_table "routes", force: :cascade do |t|
     t.string   "name"
