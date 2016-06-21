9dd43d660a4764c85abbd62d8a49127c0267c6b6
Anti-k <romanslyzebniy@gmail.com>
2016-06-21 11:56:19 +0300

    alert is removed

--- app/assets/javascripts/carriages.js
+++ app/assets/javascripts/carriages.js
@@ -1,11 +1,8 @@
 $(document).ready(function(){
   $("#carriage_type" ).change(function(e) {
-    e.preventDefault();
-    alert($("select option:selected").val());
-    var id = $("select option:selected").val();
-   
-    $('.schmidt').hide();
-    
+    e.preventDefault();    
+    var id = $("select option:selected").val();   
+    $('.schmidt').hide();    
     $('.schmidt.' + id).show();   
   });  
 });
